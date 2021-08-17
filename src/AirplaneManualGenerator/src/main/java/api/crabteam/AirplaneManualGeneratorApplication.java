9dcccd01c0ef8c15883761b393c18eefc100c9b0
Rafael Furtado <rafs.97@hotmail.com>
2021-08-16 21:05:16 -0300

    Adicionando projeto base

---
+++ src/AirplaneManualGenerator/src/main/java/api/crabteam/AirplaneManualGeneratorApplication.java
@@ -0,0 +1,13 @@
+package api.crabteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirplaneManualGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirplaneManualGeneratorApplication.class, args);
+	}
+
+}
