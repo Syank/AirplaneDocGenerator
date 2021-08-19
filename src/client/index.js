76145433f4e1629bd689559b432194aa97e39b15
Carolina M <carolinamargiott2i@gmail.com>
2021-08-19 10:02:00 -0300

    Adicionado Electron

---
+++ src/client/index.js
@@ -0,0 +1,11 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+
+    });
+
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
