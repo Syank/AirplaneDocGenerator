9f8cb7ee29d89796a71e422cda41eb9c24f9dfb2
Carolina M <carolinamargiott2i@gmail.com>
2021-09-05 13:51:02 -0300

    toques finais no componente cardHeader

--- src/client/airplanedocgenerator/src/App.js
+++ src/client/airplanedocgenerator/src/App.js
@@ -3,7 +3,10 @@ import CardHeader from "./assets/components/CardHeader";
 function App() {
     return (
         <div className="App">
-            <CardHeader></CardHeader>
+            <CardHeader
+                title="Criação de manual"
+                description="Na página inicial você tem acesso a todos os serviços para criar, compor e gerar um manual, que podem ser acessados através de seus respectivos caminhos logo abaixo"
+            ></CardHeader>
         </div>
     );
 }
--- src/client/airplanedocgenerator/src/assets/components/CardHeader.js
+++ src/client/airplanedocgenerator/src/assets/components/CardHeader.js
@@ -3,19 +3,18 @@ import React from "react";
 class CardHeader extends React.Component {
     render() {
         return (
-            <div class="container mt-4 mx-auto ">
-                <div class=" grid grid-cols-3 ">
-                    <div class="card m-2 shadow-card cursor-pointer border border-white text-center bg-white transform hover:-translate-y-1 transition-all duration-200">
-                        <div class="m-3">
-                            <h2 class="font-roboto font-bold text-lg mb-2 text-accent text-3xl pt-4">
-                                Criação de manual
-                            </h2>
-                            <p class="font-roboto text-sm underline font-bold p-6">
-                                Aqui é possível criar um novo projeto de manual,
-                                seja seguindo o guia para criar um do zero ou
-                                fazendo a importação de uma estrutura válida
-                                direto de sua máquina
-                            </p>
+            <div class="container mt-4 mx-auto">
+                <div class="grid grid-cols-3">
+                    <div class="shadow-lg cursor-pointer transform hover:-translate-y-1 transition-all duration-200">
+                        <div class="card m-2 shadow-card border border-white text-center bg-white">
+                            <div class="m-3 card">
+                                <h2 class="font-roboto font-bold text-lg mb-2 text-accent text-3xl pt-4">
+                                    {this.props.title}
+                                </h2>
+                                <p class="font-roboto text-sm underline font-bold p-6">
+                                    {this.props.description}
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
