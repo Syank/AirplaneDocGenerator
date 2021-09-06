cec62938a6ba9199823bc77609eac2f99b292fa2
Rafael Furtado <rafs.97@hotmail.com>
2021-09-06 11:05:55 -0300

    Novas configurações de estilos do Tailwind

--- src/client/airplanedocgenerator/tailwind.config.js
+++ src/client/airplanedocgenerator/tailwind.config.js
@@ -5,9 +5,24 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
+            blur: {
+                blurLogin: "2px"
+            },
+            boxShadow: {
+                simpleShadow: "3px 2px 3px rgba(0, 0, 0, 0.25)",
+                loginFormShadow: "10px 10px 0px -2px rgba(102,126,234,0.5)",
+                topBarShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)"
+            },
+            width: {
+              loginFormW: "300px"
+            },
+            height:{
+                loginFormH: "400px"
+            },
             colors: {
                 accent: "#5E74D6",
                 white: "#FFFFFF",
+                loginIcon: "#5E74D6"
             },
             backgroundColor: {
                 topBar: "#5E74D6",
