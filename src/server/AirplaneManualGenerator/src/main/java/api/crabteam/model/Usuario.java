84122b59183f3770e6b1da3251e4f97e91c7cccb
fCardosoNeto <cardoso19042000@gmail.com>
2021-09-07 19:39:40 -0300

    Update Usuario
    Remoção de itens

--- src/server/AirplaneManualGenerator/src/main/java/api/crabteam/model/Usuario.java
+++ src/server/AirplaneManualGenerator/src/main/java/api/crabteam/model/Usuario.java
@@ -1,22 +1,5 @@
 package api.crabteam.model;
 
-<<<<<<< Updated upstream
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-public abstract class Usuario {
-	
-	private Long id;
-	private String nome;
-	private String email;
-	private String senha;
-	private String tipo;
-	
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-=======
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -43,7 +26,6 @@ public abstract class Usuario {
 	@Column(nullable = false)
 	private String senha;
 		
->>>>>>> Stashed changes
 	public Long getId() {
 		return id;
 	}
@@ -68,7 +50,6 @@ public abstract class Usuario {
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-<<<<<<< Updated upstream
 	public String getTipo() {
 		return tipo;
 	}
@@ -76,7 +57,4 @@ public abstract class Usuario {
 		this.tipo = tipo;
 	}
 
-=======
-	
->>>>>>> Stashed changes
 }
