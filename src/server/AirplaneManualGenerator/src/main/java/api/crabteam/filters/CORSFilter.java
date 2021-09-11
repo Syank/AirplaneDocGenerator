702646bcb3cdc65434998885742d7000998fb258
Rafael Furtado <rafs.97@hotmail.com>
2021-09-11 10:37:51 -0300

    Adicionando filtro de política de CORS
    Este filtro permite com que o cliente aceite a resposta do servidor

---
+++ src/server/AirplaneManualGenerator/src/main/java/api/crabteam/filters/CORSFilter.java
@@ -0,0 +1,38 @@
+package api.crabteam.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+
+
+/**
+ * Filtro de política de CORS, para permitir que a aplicação cliente receba a resposta do servidor
+ * 
+ * @author Rafael Furtado
+ */
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CORSFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		
+		httpResponse.addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+		
+		chain.doFilter(request, response);
+		
+	}
+
+}
