454a37ad90cdb0ad518806b722d0ef126f1970f1
Bárbara Port <barbara.port@outlook.com>
2021-09-12 19:43:14 -0300

    início dos testes do controller do projeto

---
+++ src/server/AirplaneManualGenerator/src/test/java/api/crabteam/controllers/ProjetoControllerTest.java
@@ -0,0 +1,15 @@
+package api.crabteam.controllers;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class ProjetoControllerTest {
+
+	private static String testProjectName = "ABC-1234";
+	private static String testUserDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et felis a tortor molestie mattis. Suspendisse bibendum mauris quis felis finibus semper. Duis sagittis nisi quis tempus mollis. Mauris aliquet velit enim, nec tincidunt elit dictum eget. Donec bibendum sit amet diam quis elementum. Etiam eget mi non eros cursus suscipit. Aenean risus felis, viverra vel velit vitae, tempor varius tellus. Sed volutpat ante eu tempus ullamcorper. Cras vel pulvinar eros. Nunc vitae blandit metus. Sed pulvinar nisi a orci vestibulum bibendum. Pellentesque tempor egestas nisl. Vivamus iaculis elit interdum aliquet rhoncus. In tempus luctus quam a posuere. Vestibulum dictum lectus id est facilisis, vel bibendum tellus pretium.";
+	
+	
+}
