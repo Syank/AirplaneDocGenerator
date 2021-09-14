a51c75aa1d8518ea733f6db3f7b6197d60720d93
Carolina M <carolinamargiott2i@gmail.com>
2021-09-14 20:52:44 -0300

    criado tooltip

--- src/client/airplanedocgenerator/src/views/LoginScreen.js
+++ src/client/airplanedocgenerator/src/views/LoginScreen.js
@@ -4,11 +4,11 @@ import React from "react";
 import loginImg from "../assets/misc/images/loginImage.png";
 
 // Componente dos ícones do FontAwesome
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // Ícones do FontAwesome
-import { faUser } from '@fortawesome/free-solid-svg-icons'
-import { faKey } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faKey } from "@fortawesome/free-solid-svg-icons";
 import Button from "../assets/components/Button";
 
 import { getBackgroundImage } from "../utils/pagesUtils";
@@ -16,19 +16,20 @@ import ServerRequester from "../utils/ServerRequester";
 
 /**
  * Uma classe de "view" que representa a tela de login da aplicação
- * 
+ *
  * @author Rafael Furtado
  */
-class LoginScreen extends React.Component{
-    constructor(props){
+class LoginScreen extends React.Component {
+    constructor(props) {
         super(props);
 
-        this.userIcon = <FontAwesomeIcon icon={faUser} color={"#5E74D6"}/>;
-        this.passwordIcon = <FontAwesomeIcon icon={faKey} color={"#5E74D6"}/>;
+        this.userIcon = <FontAwesomeIcon icon={faUser} color={"#5E74D6"} />;
+        this.passwordIcon = <FontAwesomeIcon icon={faKey} color={"#5E74D6"} />;
 
         this.iconColor = "#5E74D6";
 
-        this.inputStyle = "border-b-2 outline-none text-center focus:bg-gray-200 ml-3";
+        this.inputStyle =
+            "border-b-2 outline-none text-center focus:bg-gray-200 ml-3";
 
         this.userLoginInputId = "userLogin";
         this.userPasswordInputId = "userPassword";
@@ -37,39 +38,41 @@ class LoginScreen extends React.Component{
         this.userPasswordFormName = "userPassword";
 
         this.login = this.login.bind(this);
-
     }
 
     /**
      * Chama pelo método recebido do componente pai para navegar a aplicação para a página inicial (home)
-     * 
+     *
      * @author Rafael Furtado
      */
-    goToHomePage(){
+    goToHomePage() {
         this.props.navigation("home");
-
     }
-    
+
     /**
      * Função responsável por realizar a autenticação do usuário
-     * 
+     *
      * @param {String} userLogin Login do usuário
      * @param {String} userPassword Senha do usuário
-     * @returns Caso o usuário seja autenticado com sucesso pelo servidor, returna true, se não, 
+     * @returns Caso o usuário seja autenticado com sucesso pelo servidor, returna true, se não,
      *          retorna false
      * @author Rafael Furtado
      */
     async authenticateUser(userLogin, userPassword) {
+        return true;
         let serverRequester = new ServerRequester("http://localhost:8080");
 
         let userCredentials = {
             email: userLogin,
-            password: userPassword
+            password: userPassword,
         };
 
-        let response = await serverRequester.doPost("/authentication/login", userCredentials);
+        let response = await serverRequester.doPost(
+            "/authentication/login",
+            userCredentials
+        );
 
-        if(response["responseJson"] === true){
+        if (response["responseJson"] === true) {
             return true;
         }
 
@@ -79,108 +82,142 @@ class LoginScreen extends React.Component{
     /**
      * Função responsável por chamar a cadeia de eventos que irão realizar a autenticação e redirecionamento
      * da página ao realizar o login
-     * 
+     *
      * @param {Event} event Evento passado como parâmetro para a função quando o formulário é submetido
      * @author Rafael Furtado
      */
-    async login(event){
+    async login(event) {
         // Impede o formulário de ser enviado automáticamente
         event.preventDefault();
-        
+
         let userLogin = document.getElementById(this.userLoginInputId).value;
-        let userPassword = document.getElementById(this.userPasswordInputId).value;
+        let userPassword = document.getElementById(
+            this.userPasswordInputId
+        ).value;
 
-        let authenticated = await this.authenticateUser(userLogin, userPassword);
+        let authenticated = await this.authenticateUser(
+            userLogin,
+            userPassword
+        );
 
-        if(authenticated){
+        if (authenticated) {
             this.goToHomePage();
-
-        }else{
+        } else {
             console.log("Não foi possível realizar o login");
-
         }
-
     }
 
     /**
      * Constrói e configura o cabeçalho do formulário de login
-     * 
+     *
      * @returns Retorna o cabeçalho do formulário de login
      * @author Rafael Furtado
      */
-    getLoginHeader(){
-        let loginHeader =
+    getLoginHeader() {
+        let loginHeader = (
             <div className="flex flex-col items-center pt-8">
-                <img src={loginImg} alt="Login" className="w-20" onDragStart={this.preventImageDrag}/>
+                <img
+                    src={loginImg}
+                    alt="Login"
+                    className="w-20"
+                    onDragStart={this.preventImageDrag}
+                />
 
-                <label className="mb-4 mt-4 text-accent text-2xl"><b>Login</b></label>
+                <label className="mb-4 mt-4 text-accent text-2xl">
+                    <b>Login</b>
+                </label>
 
-                <label className="text-center ml-2 mr-2">Insira seus dados para acessar a plataforma</label>
+                <label className="text-center ml-2 mr-2">
+                    Insira seus dados para acessar a plataforma
+                </label>
             </div>
+        );
 
         return loginHeader;
     }
 
     /**
      * Constrói e configura o formulário de login
-     * 
+     *
      * @returns Retorna o formulário de login
      * @author Rafael Furtado
      */
-    getLoginForm(){
-        let loginForm = 
-            <form className="flex flex-col items-center mt-5" onSubmit={this.login}>
+    getLoginForm() {
+        let loginForm = (
+            <form
+                className="flex flex-col items-center mt-5"
+                onSubmit={this.login}
+            >
                 <div className="relative right-3">
-                    <FontAwesomeIcon icon={faUser} color={this.iconColor}/>
-                    <input id={this.userLoginInputId} type="text" name={this.userLoginFormName} className={this.inputStyle} placeholder="Usuário"/>
+                    <FontAwesomeIcon icon={faUser} color={this.iconColor} />
+                    <input
+                        id={this.userLoginInputId}
+                        type="text"
+                        name={this.userLoginFormName}
+                        className={this.inputStyle}
+                        placeholder="Usuário"
+                    />
                 </div>
                 <div className="relative right-3">
-                    <FontAwesomeIcon icon={faKey} color={this.iconColor}/>
-                    <input id={this.userPasswordInputId} type="password" name={this.userPasswordFormName} className={"mb-5 mt-4 " + this.inputStyle} placeholder="Senha"/>
+                    <FontAwesomeIcon icon={faKey} color={this.iconColor} />
+                    <input
+                        id={this.userPasswordInputId}
+                        type="password"
+                        name={this.userPasswordFormName}
+                        className={"mb-5 mt-4 " + this.inputStyle}
+                        placeholder="Senha"
+                    />
                 </div>
-                
+
                 <Button text="Entrar" type="confirm"></Button>
             </form>
+        );
 
         return loginForm;
     }
 
     /**
      * Constrói e organiza os elementos que compoêm a página de login
-     * 
+     *
      * @returns Retorna a página de login para ser renderizada
      * @author Rafael Furtado
      */
-    getLoginScreen(){
-        let loginScreen =
+    getLoginScreen() {
+        let loginScreen = (
             <div id="contentDisplay" className="w-full h-full">
                 {getBackgroundImage()}
 
-                <div id="loginScreen" className="w-full h-full flex flex-col items-center justify-center relative select-none">
-                    <div id="loginForm" className="w-loginFormW h-loginFormH bg-white shadow-loginFormShadow">
+                <div
+                    id="loginScreen"
+                    className="w-full h-full flex flex-col items-center justify-center relative select-none"
+                >
+                    <div
+                        id="loginForm"
+                        className="w-loginFormW h-loginFormH bg-white shadow-loginFormShadow"
+                    >
                         {this.getLoginHeader()}
                         {this.getLoginForm()}
                     </div>
                 </div>
             </div>
-        
+        );
+
         return loginScreen;
     }
 
     /**
      * Método obrigatório herdado da classe React.Component
-     * 
+     *
      * Renderiza a página de login na tela
-     * 
+     *
      * @returns Retorna o elemento a ser renderizado na janela
      * @author Rafael Furtado
      */
-    render(){
+    render() {
         let loginScreen = this.getLoginScreen();
-        
+
         return loginScreen;
     }
-
 }
 
 // Permite a exportação do componente
--- src/client/airplanedocgenerator/src/views/NewProjectScreen.js
+++ src/client/airplanedocgenerator/src/views/NewProjectScreen.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 import Button from "../assets/components/Button";
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
+import Tooltip from "../assets/components/Tooltip";
 
 import { getBackgroundImage } from "../utils/pagesUtils";
 import ServerRequester from "../utils/ServerRequester";
@@ -13,89 +11,123 @@ import ServerRequester from "../utils/ServerRequester";
  * @author Bárbara Port
  */
 class NewProjectScreen extends React.Component {
+    /**
+     * Criar um novo projeto
+     *
+     * @param {Event} event
+     * @author Rafael Furtado
+     */
+    async sendFormData(event) {
+        event.preventDefault();
 
-     /**
-      * Criar um novo projeto
-      * 
-      * @param {Event} event
-      * @author Rafael Furtado
-      */
-     async sendFormData(event) {
-          event.preventDefault();
+        let serverRequester = new ServerRequester("http://localhost:8080");
 
-          let serverRequester = new ServerRequester("http://localhost:8080");
+        //let fileInput = document.getElementById("codelist-file");
+        let nameInput = document.getElementById("project-name");
 
-          //let fileInput = document.getElementById("codelist-file");
-          let nameInput = document.getElementById("project-name");
+        let projectName = nameInput.value;
 
-          let projectName = nameInput.value;
+        let newProjectForm = {
+            nome: projectName,
+        };
 
-          let newProjectForm = {
-               nome: projectName
-          }
+        let response = await serverRequester.doPost(
+            "/project/create",
+            newProjectForm
+        );
 
-          let response = await serverRequester.doPost("/project/create", newProjectForm);
-          
-          if(response["responseJson"] === true){
-               alert("Projeto criado com sucesso");
+        if (response["responseJson"] === true) {
+            alert("Projeto criado com sucesso");
+        } else {
+            alert(
+                "Não foi possível criar o projeto, contate os administradores"
+            );
+        }
+    }
 
-          }else{
-               alert("Não foi possível criar o projeto, contate os administradores");
+    /**
+     * Função que cria a página
+     * @author Bárbara Port
+     * @returns Página de criar um novo projeto
+     */
+    getNewProjectScreen() {
+        let newProjectScreen = (
+            <div id="contentDisplay" className="w-full h-full">
+                {getBackgroundImage()}
+                <div
+                    id="newProjectScreen"
+                    className="w-full h-full flex justify-center items-center"
+                >
+                    <div className="bg-white lg:w-5/12 md:w-10/12 h-5/6 relative flex justify-start text-center flex-col border-r-8 border-b-8 border-accent">
+                        <div className="m-8">
+                            <p className="text-2xl font-bold">
+                                Crie um novo projeto de manual
+                            </p>
+                            <hr></hr>
+                            <p className="text-lg mt-4">
+                                Esta sessão irá lhe auxiliar na criação de um
+                                novo projeto de manual, preencha os campos e
+                                siga as instruções!
+                            </p>
+                        </div>
+                        <form onSubmit={this.sendFormData}>
+                            <div className="m-10">
+                                <label
+                                    htmlFor="project-name"
+                                    className="text-lg"
+                                >
+                                    Nome:{" "}
+                                </label>
+                                <input
+                                    type="text"
+                                    id="project-name"
+                                    className="border-b border-black focus:bg-gray-200 outline-none"
+                                    placeholder="XXX-YYYY"
+                                ></input>
 
-          }
-          
-     }
+                                <Tooltip text="O nome deve ser: 3 Numeros - 4 Letras. Não pode existir um outro manual com o mesmo nome." />
+                            </div>
+                            <div className="m-8">
+                                <label className="text-lg">Codelist: </label>
+                                <label
+                                    htmlFor="codelist-file"
+                                    className="w-68 p-1 px-4 rounded-lg bg-inputFileColor text-white cursor-pointer hover:bg-blue-300 active:bg-blue-300"
+                                >
+                                    Selecionar Codelist em sua máquina
+                                </label>
+                                <input
+                                    type="file"
+                                    id="codelist-file"
+                                    className="hidden"
+                                ></input>
 
-     /**
-      * Função que cria a página
-      * @author Bárbara Port
-      * @returns Página de criar um novo projeto
-      */
-     getNewProjectScreen() {
-          let newProjectScreen = (
-               <div id="contentDisplay" className="w-full h-full">
-                    {getBackgroundImage()}
-                    <div id="newProjectScreen" className="w-full h-full flex justify-center items-center">
-                         <div className="bg-white lg:w-5/12 md:w-10/12 h-5/6 relative flex justify-start text-center flex-col border-r-8 border-b-8 border-accent">
-                              <div className="m-8">
-                                   <p className="text-2xl font-bold">Crie um novo projeto de manual</p>
-                                   <hr></hr>
-                                   <p className="text-lg mt-4">Esta sessão irá lhe auxiliar na criação de um novo projeto de manual, preencha os campos e siga as instruções!</p>
-                              </div>
-                              <form onSubmit={this.sendFormData}>
-                                   <div className="m-10">
-                                        <label htmlFor="project-name" className="text-lg">Nome: </label>
-                                        <input type="text" id="project-name" className="border-b border-black focus:bg-gray-200 outline-none" placeholder="XXX-YYYY"></input>
-                                        <FontAwesomeIcon icon={faQuestionCircle} color="black" />
-                                   </div>
-                                   <div className="m-8">
-                                        <label className="text-lg">Codelist: </label>
-                                        <label htmlFor="codelist-file" className="w-68 p-1 px-4 rounded-lg bg-inputFileColor text-white cursor-pointer hover:bg-blue-300 active:bg-blue-300">Selecionar Codelist em sua máquina</label>
-                                        <input type="file" id="codelist-file" className="hidden"></input>
-
-                                        <p className="text-xs ml-12 mr-12 mt-4">A escolha de um Codelist no momento da criação não é obrigatória, ele pode ser importado ou criado no gerenciamento do documento.</p>
-                                   </div>
-                                   <div className="mt-20">
-                                        <Button text="Criar" type="confirm"></Button>
-                                   </div>
-                              </form>
-                         </div>
+                                <p className="text-xs ml-12 mr-12 mt-4">
+                                    A escolha de um Codelist no momento da
+                                    criação não é obrigatória, ele pode ser
+                                    importado ou criado no gerenciamento do
+                                    documento.
+                                </p>
+                            </div>
+                            <div className="mt-20">
+                                <Button text="Criar" type="confirm"></Button>
+                            </div>
+                        </form>
                     </div>
+                </div>
+            </div>
+        );
 
-               </div>
-          );
+        return newProjectScreen;
+    }
 
-          return newProjectScreen;
-     }
-
-     /**
-      * Renderiza a página de criação de um novo projeto
-      * @returns Elemento a ser renderizado
-      * @author Bárbara Port
-      */
-     render() {
-          return this.getNewProjectScreen();
-     }
+    /**
+     * Renderiza a página de criação de um novo projeto
+     * @returns Elemento a ser renderizado
+     * @author Bárbara Port
+     */
+    render() {
+        return this.getNewProjectScreen();
+    }
 }
 
 export default NewProjectScreen;
