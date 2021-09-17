e243534497fa836984941f1ff536a29c44901b69
Rafael Furtado <rafs.97@hotmail.com>
2021-09-17 08:43:32 -0300

    Adicionando documentação restante

--- src/client/airplanedocgenerator/src/assets/components/SelectProjectItem.js
+++ src/client/airplanedocgenerator/src/assets/components/SelectProjectItem.js
@@ -3,6 +3,14 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
+
+
+/**
+ * Classe de componente para construir um elemento da lista de manuais a serem exibidos na página
+ * SelectProjectScreen
+ * 
+ * @author Rafael Furtado
+ */
 class SelectProjectItem extends React.Component {
     constructor(props) {
         super(props);
@@ -14,16 +22,30 @@ class SelectProjectItem extends React.Component {
 
     }
 
+    /**
+     * Seleciona o manual para exibir suas informações
+     * 
+     * @author Rafael Furtado
+     */
     setSelected() {
         this.props.setSelectedItem(this.nome);
 
     }
 
+    /**
+     * Delete o manual em questão do sistema
+     */
     delete(){
         this.props.deleteProject(this.nome);
 
     }
 
+    /**
+     * Constrói o componente que representa um manual na lista de exibição de manuais
+     * 
+     * @returns Retorna o componente pronto para ser renderizado
+     * @author Rafael Furtado
+     */
     getSelectItem(){
         let item = (
             <div className="flex flex-row justify-between pr-3 pl-3 border-b-2 border-gray-400 mb-5">
@@ -40,6 +62,14 @@ class SelectProjectItem extends React.Component {
         return item;
     }
 
+    /**
+     * Método obrigatório herdado da classe React.Component
+     *
+     * Renderiza o elemento na página
+     *
+     * @returns Retorna o elemento a ser renderizado na janela
+     * @author Rafael Furtado
+     */
     render() {
         let item = this.getSelectItem();
 
