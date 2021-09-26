e2ac82d9136fb052405b613496f6bf49e50a94e7
Rafael Furtado <rafs.97@hotmail.com>
2021-09-26 16:52:45 -0300

    Removendo criação de projetos de teste

--- src/server/AirplaneManualGenerator/src/main/java/api/crabteam/AirplaneManualGeneratorApplication.java
+++ src/server/AirplaneManualGenerator/src/main/java/api/crabteam/AirplaneManualGeneratorApplication.java
@@ -46,62 +46,7 @@ public class AirplaneManualGeneratorApplication implements CommandLineRunner {
 			userRep.save(admin);
 			
 		}
-		
-		Projeto projeto1 = new Projeto("ABO-1234", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ex massa, posuere quis massa sed", null);
-		Projeto projeto2 = new Projeto("AOC-2851", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ex massa, posuere quis massa sed, bibendum fringilla purus. Vivamus est turpis, semper sit amet augue non", null);
-		Projeto projeto3 = new Projeto("ATC-7412", "Vivamus est turpis, semper sit amet augue non", null);
-		Projeto projeto4 = new Projeto("ACC-5233", "consectetur adipiscing elit. Nulla ex massa, posuere quis massa sed", null);
-		Projeto projeto5 = new Projeto("BCC-1235", "posuere quis massa sed, bibendum fringilla purus. Vivamus est", null);
-		Projeto projeto6 = new Projeto("ADC-2686", "Vivamus est turpis, semper sit amet augue non", null);
-		Projeto projeto7 = new Projeto("ZDC-1987", "consectetur adipiscing elit. Nulla ex massa, posuere quis massa sed", null);
-		
-		projRep.save(projeto1);
-		projRep.save(projeto2);
-		projRep.save(projeto3);
-		projRep.save(projeto4);
-		projRep.save(projeto5);
-		projRep.save(projeto6);
-		projRep.save(projeto7);
-		
-		
-		Projeto projeto = new Projeto("ABY-1234", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ex massa, posuere quis massa sed", null);
-		
-		Codelist codelist = new Codelist();
-		codelist.setNome("ABY-1234");
-		
-		Linha linha = new Linha();
-		linha.setBlock("00");
-		linha.setBlockName("Letter");
-		linha.setCode("00");
-		linha.setFilePath("Path//to:c//file.txt");
-		linha.setSecao("01");
-		linha.setSubSecao("002");
-		
-		Remark remark = new Remark();
-		remark.setApelido("Mars");
-		remark.setTraco("-50");
-		
-		ArrayList<Remark> remarkList = new ArrayList<Remark>();
-		remarkList.add(remark);
-		
-		ArrayList<Linha> linhaList = new ArrayList<Linha>();
-		linhaList.add(linha);
-		
-		linha.setRemarks(remarkList);
-		
-		codelist.setLinhas(linhaList);
-		
-		projeto.setCodelist(codelist);
-		
-		projRep.save(projeto);
-		
-		Projeto proj = projRep.findByName("ABY-1234");
-		
-		System.out.println(proj.getNome());
-		System.out.println(proj.getCodelist().getNome());
-		System.out.println(proj.getCodelist().getLinhas().get(0).getFilePath());
-		System.out.println(proj.getCodelist().getLinhas().get(0).getRemarks().get(0).getApelido());
-		
+
 	}
 
 }
