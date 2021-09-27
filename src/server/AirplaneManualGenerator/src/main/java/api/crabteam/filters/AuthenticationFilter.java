ed990ada759f914455bd7ae0df1f827eeb9d137a
Rafael Furtado <rafs.97@hotmail.com>
2021-09-27 20:46:12 -0300

    Corrigindo acesso ao Swagger

--- src/server/AirplaneManualGenerator/src/main/java/api/crabteam/filters/AuthenticationFilter.java
+++ src/server/AirplaneManualGenerator/src/main/java/api/crabteam/filters/AuthenticationFilter.java
@@ -33,7 +33,8 @@ public class AuthenticationFilter implements Filter {
 		boolean authentication = requestPath.contains("/authentication/login");
 		boolean checkLogged = requestPath.equals("/user/isLogged");
 		boolean preflight = httpRequest.getMethod().equalsIgnoreCase("OPTIONS");
-		boolean swagger = requestPath.contains("/swagger-ui");
+		boolean swagger = requestPath.contains("swagger");
+		boolean swaggerDocs = requestPath.contains("api-docs");
 
 		boolean allowRequest = false;
 		
@@ -41,7 +42,7 @@ public class AuthenticationFilter implements Filter {
 			if(preflight) {
 				allowRequest = true;
 				
-			}else if(authentication || checkLogged || swagger) {
+			}else if(authentication || checkLogged || swagger || swaggerDocs) {
 				allowRequest = true;
 				
 			}
