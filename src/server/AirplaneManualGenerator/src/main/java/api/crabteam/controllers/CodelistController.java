8f7abcd769a02354391321660bda188d5e1c2a99
Bárbara Port <barbara.port@outlook.com>
2021-09-27 19:30:58 -0300

    finalizando controller da codelist
    obs.: ainda não testei

--- src/server/AirplaneManualGenerator/src/main/java/api/crabteam/controllers/CodelistController.java
+++ src/server/AirplaneManualGenerator/src/main/java/api/crabteam/controllers/CodelistController.java
@@ -1,17 +1,18 @@
 package api.crabteam.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import api.crabteam.controllers.requestsBody.NewCodelist;
 import api.crabteam.model.entities.builders.CodelistBuilder;
 import api.crabteam.model.repositories.CodelistRepository;
-
-import org.springframework.web.bind.annotation.RequestBody;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -37,12 +38,19 @@ public class CodelistController {
     @ApiOperation("Creates a new codelist by uploading it.")
 	@ApiResponses({
         @ApiResponse(code = 200, message = "Codelist successfully created."),
-        @ApiResponse(code = 404, message = "Codelist wasn't created.")
+        @ApiResponse(code = 400, message = "Codelist wasn't created.")
     })
-	public void uploadCodelist (@RequestBody NewCodelist newCodelist) {
+	public ResponseEntity<?> uploadCodelist (@RequestBody NewCodelist newCodelist, @PathVariable String projectName) {
 		codelistBuilder.setCodelistRepository(codelistRepository);
+		codelistBuilder.build(newCodelist, projectName);
 		
+		if(codelistBuilder.isPersisted()) {
+			return new ResponseEntity<Boolean>(true, HttpStatus.OK);
+		}
 		
+		String failMessage = codelistBuilder.getFailMessage();
+		
+		return new ResponseEntity<String>(failMessage, HttpStatus.BAD_REQUEST);
 	}
 
 }
