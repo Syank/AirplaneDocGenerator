8a69389c244ed782f642d5792f5433480c23ff31
Bárbara Port <barbara.port@outlook.com>
2021-09-28 21:35:03 -0300

    adição de uma linha

--- src/server/AirplaneManualGenerator/src/main/java/api/crabteam/controllers/LinhaController.java
+++ src/server/AirplaneManualGenerator/src/main/java/api/crabteam/controllers/LinhaController.java
@@ -1,13 +1,19 @@
 package api.crabteam.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import antlr.debug.NewLineListener;
 import api.crabteam.controllers.requestsBody.NewLine;
+import api.crabteam.model.entities.Codelist;
+import api.crabteam.model.entities.Linha;
+import api.crabteam.model.entities.builders.LinhaBuilder;
 import api.crabteam.model.repositories.CodelistRepository;
 import api.crabteam.model.repositories.LinhaRepository;
 import io.swagger.annotations.Api;
@@ -31,13 +37,43 @@ public class LinhaController {
 	@Autowired
 	LinhaRepository linhaRepository;
 	
+	@Autowired
+	LinhaBuilder linhaBuilder;
+	
 	@PostMapping("/new/{codelistName}")
 	@ApiOperation("Creates a new line into a codelist.")
 	@ApiResponses({
         @ApiResponse(code = 200, message = "Line successfully created."),
         @ApiResponse(code = 400, message = "Line wasn't created.")
     })
-	public void createLine (@RequestBody NewLine newLine, @PathVariable String codelistName) {
+	public ResponseEntity<?> createLine (@RequestBody NewLine newLine, @PathVariable String codelistName) {
+		
+		Codelist codelist = codelistRepository.findByName(codelistName);
+		
+		Linha linha = new Linha();
+		linha.setSectionNumber(newLine.getSectionNumber());
+		linha.setSubsectionNumber(newLine.getSubsectionNumber());
+		linha.setBlockNumber(newLine.getBlockNumber());
+		linha.setBlockName(newLine.getBlockName());
+		linha.setCode(newLine.getCode());
+		linha.setFilePath(newLine.getFilePath());
+		linha.setRemarks(newLine.getRemarks());
+		
+		try {
+			linhaRepository.save(linha);
+			try {
+				codelist.addLinha(linha);
+				codelistRepository.save(codelist);
+			}
+			catch (Exception e) {
+				return new ResponseEntity<String>("A linha não foi adicionada à codelist.", HttpStatus.BAD_REQUEST);
+			}
+		}
+		catch (Exception e) {
+			return new ResponseEntity<String>("A linha não foi criada.", HttpStatus.BAD_REQUEST);
+		}
+		
+		return new ResponseEntity<Boolean>(true, HttpStatus.OK);
 		
 	}
 
--- src/server/AirplaneManualGenerator/src/main/java/api/crabteam/model/repositories/CodelistRepository.java
+++ src/server/AirplaneManualGenerator/src/main/java/api/crabteam/model/repositories/CodelistRepository.java
@@ -1,9 +1,12 @@
 package api.crabteam.model.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import api.crabteam.model.entities.Codelist;
 
 public interface CodelistRepository extends JpaRepository<Codelist, Integer> {
 
+	@Query("select codelist from codelist c where c.nome = ?1")
+	Codelist findByName(String name);
 }
