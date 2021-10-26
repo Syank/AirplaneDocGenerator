196e4727f7e512be1ba912f34326518836723bc5
Bárbara Port <barbara.port@outlook.com>
2021-10-26 13:10:35 -0300

    exibindo todas as linhas da codelist
    exceto remarks, que precisam de uma atenção

--- src/server/AirplaneManualGenerator/src/main/java/api/crabteam/utils/CodelistExporter.java
+++ src/server/AirplaneManualGenerator/src/main/java/api/crabteam/utils/CodelistExporter.java
@@ -3,6 +3,7 @@ package api.crabteam.utils;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.JFileChooser;
@@ -26,13 +27,58 @@ import api.crabteam.model.repositories.CodelistRepository;
  */
 public class CodelistExporter {
 	
+	private static CodelistColumn[] getCodelistColumns () {
+		return CodelistColumn.getValues();
+	}
+	
 	private static void createCodelistHeader (Sheet sheet) {
 		Row header = sheet.createRow(0);
-		CodelistColumn[] codelistColumns = CodelistColumn.getValues();
+		CodelistColumn[] codelistColumns = getCodelistColumns();
 		for (int i = 0; i < codelistColumns.length; i++) {
 			String cellValue = codelistColumns[i].columnType;
 			Cell headerCell = header.createCell(i);
 			headerCell.setCellValue(cellValue);
+		}
+	}
+	
+	public static void createCodelistLines (Sheet sheet, List<Linha> codelistLines) {
+		
+		for (int l = 1; l <= codelistLines.size(); l++) {
+			int i = l - 1;
+			Row line = sheet.createRow(l);
+			CodelistColumn[] codelistColumns = getCodelistColumns();
+			for (int c = 0; c < codelistColumns.length; c++) {
+				Cell columnCell = line.createCell(c);
+				String cellContent = "Empty";
+				String cellType = codelistColumns[c].columnType;
+				switch (cellType) {
+				case "Nº SEÇÃO":
+					cellContent = codelistLines.get(i).getSectionNumber();
+					break;
+				case "SEÇÃO":
+					cellContent = codelistLines.get(i).getSectionName();
+					break;
+				case "Nº SUB SEÇÃO":
+					cellContent = codelistLines.get(i).getSubsectionNumber();
+					break;
+				case "SUB SEÇÃO":
+					cellContent = codelistLines.get(i).getSubsectionName();
+					break;
+				case "Nº BLOCK":
+					cellContent = codelistLines.get(i).getBlockNumber();
+					break;
+				case "BLOCK NAME":
+					cellContent = codelistLines.get(i).getBlockName();
+					break;
+				case "CODE":
+					cellContent = codelistLines.get(i).getCode();
+					break;
+				case "Remarks":
+					//cellContent = codelistLines.get(i).getRemarks();
+					break;
+				}
+				columnCell.setCellValue(cellContent);
+			}
 		}
 	}
 	
@@ -42,6 +88,7 @@ public class CodelistExporter {
 		Sheet sheet = workbook.createSheet(codelistName);
 		
 		createCodelistHeader(sheet);
+		createCodelistLines(sheet, codelistLines);
 		
 		String fileLocation = "C:\\Users\\port3\\Desktop\\exportedCodelistTest.xlsx";
 		FileOutputStream outputStream = new FileOutputStream(fileLocation);
