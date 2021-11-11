b9cb4136fcbdbd3c1ecb7ca78ab03e02e38f1eaa
Bárbara Port <barbara.port@outlook.com>
2021-11-11 00:28:27 -0300

    lógica básica para a geração do manual delta

--- src/server/AirplaneManualGenerator/src/main/java/api/crabteam/utils/DeltaManualHelper.java
+++ src/server/AirplaneManualGenerator/src/main/java/api/crabteam/utils/DeltaManualHelper.java
@@ -1,5 +1,63 @@
 package api.crabteam.utils;
 
-public class DeltaManualHelper {
+import java.util.List;
 
+import api.crabteam.model.entities.Linha;
+import api.crabteam.model.entities.Projeto;
+import api.crabteam.model.entities.Remark;
+import api.crabteam.model.entities.Revisao;
+
+public class DeltaManualHelper {
+	
+	Projeto project;
+	
+	static String deltaLetter;
+	static String deltaCover;
+	static String deltaLEP;
+	
+	static List<String> remainingPages;
+	
+	public DeltaManualHelper (Projeto project) {
+		this.project = project;
+	}
+	
+	private static List<Linha> getLastRevisionLines (Projeto project) {
+		Revisao revision = project.getLastRevision();
+		return revision.getLinhas();
+	}
+	
+	private static void arrangePages (Projeto project, List<Linha> lines, Remark remark) {
+		int selectedRemarkID = remark.getId();
+		String projectName = project.getNome();
+		String sFilePath = "";
+		for (Linha line : lines) {
+			List<Remark> lineRemarks = line.getRemarks();
+			for (Remark r : lineRemarks) {
+				int actualRemarkID = r.getId();
+				if (selectedRemarkID == actualRemarkID) {
+					String[] result = FileVerifications.fileDestination(line, projectName);
+					sFilePath = result[0] + result[1];
+					switch (line.getBlockNumber()) {
+						case "00":
+							deltaLetter = sFilePath;
+							break;
+						case "01":
+							deltaCover = sFilePath;
+							break;
+						case "02":
+							deltaLEP = sFilePath;
+							break;
+						default:
+							remainingPages.add(sFilePath);
+							break;
+					}
+				}
+			}
+		}
+	}
+	
+	public static void generateDeltaManual (Projeto project, Remark remark) {
+		List<Linha> lastRevisionLines = getLastRevisionLines(project);
+		arrangePages(project, lastRevisionLines, remark);
+	}
 }
