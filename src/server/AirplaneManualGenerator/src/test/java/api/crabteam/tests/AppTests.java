9d701ce75f8606ea7a049a63adfe00d3b9b8ef00
Bárbara Port <barbara.port@outlook.com>
2021-11-29 18:23:34 -0300

    melhoria no código

--- src/server/AirplaneManualGenerator/src/test/java/api/crabteam/tests/AppTests.java
+++ src/server/AirplaneManualGenerator/src/test/java/api/crabteam/tests/AppTests.java
@@ -26,7 +26,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import api.crabteam.controllers.requestsBody.NewProject;
 
 /**
- * Classe responsável pelo testes que faremos para a disciplina Testes de Software
+ * Classe responsável pelo testes que faremos para a disciplina Testes de
+ * Software
+ * 
  * @author Bárbara Port
  * @author Rafael Furtado
  *
@@ -35,64 +37,80 @@ import api.crabteam.controllers.requestsBody.NewProject;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class AppTests {
-	
+
 	@Autowired
 	MockMvc mockMvc;
-	
+
 	/**
 	 * Converte o objeto recebido para um equivalente em String no formato JSON
 	 * 
 	 * @param object - Objeto a ser convertido
 	 * @return Retorna uma String JSON que representa o objeto recebido
-	 * @throws JsonProcessingException Caso não seja possível fazer o parse do objeto para uma String JSON
+	 * @throws JsonProcessingException Caso não seja possível fazer o parse do
+	 *                                 objeto para uma String JSON
 	 * @author Rafael Furtado
 	 */
 	private static String asJsonString(Object object) throws JsonProcessingException {
 		return new ObjectMapper().writeValueAsString(object);
 	}
-	
+
+	/**
+	 * Cria uma nova sessão para o contexto dos testes
+	 * 
+	 * @return Retorna uma sessão pertencente ao contexto dos teste
+	 * @author Rafael Furtado
+	 */
 	private MockHttpSession getSessionForTest() {
 		MockHttpServletRequest request = new MockHttpServletRequest();
 		RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
 		MockHttpSession session = (MockHttpSession) request.getSession(true);
 		return session;
 	}
-	
-	private NewProject newProjectHelper (String name, String description) throws IOException {
+
+	/**
+	 * Método para realizar os sets dos atributos de um novo projeto
+	 * @param name nome do projeto
+	 * @param description descrição do projeto
+	 * @return <b>NewProject</b> objeto do novo projeto
+	 * @throws IOException
+	 * @author Bárbara Port
+	 */
+	private NewProject newProjectHelper(String name, String description) throws IOException {
 		NewProject newProject = new NewProject();
 		newProject.setNome(name);
 		newProject.setDescricao(description);
 		return newProject;
 	}
-	
-	private void performCreateProjectRequest (MockMultipartFile codelistFile, NewProject newProject) throws JsonProcessingException, Exception {
+
+	private void performCreateProjectRequest(MockMultipartFile codelistFile, NewProject newProject)
+			throws JsonProcessingException, Exception {
 		MockHttpSession session = getSessionForTest();
 		String newProjectAsJsonString = asJsonString(newProject);
-		
+
 		@SuppressWarnings("unused")
-		MockHttpServletResponse result = (MockHttpServletResponse) mockMvc.perform(
-																				MockMvcRequestBuilders.multipart("/project/create")
-																				.file(codelistFile)
-																	            .contentType(MediaType.APPLICATION_JSON)
-																	            .content(newProjectAsJsonString)
-																	            .session(session)
-																            ).andExpect(status().is(200));
+		MockHttpServletResponse result = (MockHttpServletResponse) mockMvc
+				.perform(MockMvcRequestBuilders.multipart("/project/create").file(codelistFile)
+						.contentType(MediaType.APPLICATION_JSON).content(newProjectAsJsonString).session(session))
+				.andExpect(status().is(200));
 	}
-	
-//	private static void createProject (String name, String description, String codelistPath) throws IOException {
-//		NewProject np1 = newProjectHelper(name, description);
-//		MockMultipartFile codelistFile = new MockMultipartFile("codelistFile", codelistPath.getBytes());
-//		
-//		performCreateProjectRequest(codelistFile, np1);
-//	}
-	
+
+	/**
+	 * Método que efetua a criação de um projeto
+	 * @param name nome do projeto
+	 * @param description descrição do projeto
+	 * @param codelistPath caminho em que se encontra a codelist
+	 * @throws Exception
+	 * @author Bárbara Port
+	 */
+	private void createProject(String name, String description, String codelistPath) throws Exception {
+		NewProject newProject = newProjectHelper(name, description);
+		MockMultipartFile codelistFile = new MockMultipartFile("codelistFile", codelistPath.getBytes());
+		performCreateProjectRequest(codelistFile, newProject);
+	}
+
 	@Test
 	void test() throws Exception {
-		// Criando um projeto
-		NewProject np1 = newProjectHelper("ABC-1234", "Lorem ipsum");
-		MockMultipartFile codelistFile = new MockMultipartFile("codelistFile", "C:\\Users\\port3\\Desktop\\Codelist.xlsx".getBytes());
-		performCreateProjectRequest(codelistFile, np1);
-		// ------------------
+		createProject("ABC-1234", "Lorem ipsum", "C:\\Users\\port3\\Desktop\\Codelist.xlsx");
 	}
 
 }
